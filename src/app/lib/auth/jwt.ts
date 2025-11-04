cbbc79d79a4660f7a3d68fb61bd743a0e46d2b7c
holoscopecheck <holoscopecheck@gmail.com>
2025-11-04 10:16:54 +0900

    implement function for verify jwt token

--- src/app/lib/auth/jwt.ts
+++ src/app/lib/auth/jwt.ts
@@ -1,5 +1,16 @@
 import jwt from "jsonwebtoken";
 
-export function generateToken(payload: object, secret: string, option: jwt.SignOptions) {
+export function generateToken(
+  payload: object,
+  secret: string,
+  option: jwt.SignOptions
+) {
   return jwt.sign(payload, secret, option);
+}
+
+export function verifyToken(
+  token: string,
+  secret: string
+): string | jwt.JwtPayload {
+  return jwt.verify(token, secret);
 }
