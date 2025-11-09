ae3a59349aed39be019bc50da9a5e3a0d9da8bc0
holoscopecheck <holoscopecheck@gmail.com>
2025-11-09 09:29:59 +0900

    fix title

--- src/app/resultanimation/layout.tsx
+++ src/app/resultanimation/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "結果発表 - HRV",
+  title: "Hackathon Result Visualizer",
   description: "Hackathon Result Visualizer",
 };
 
